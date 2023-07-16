c442636ce35027acb4d6b1b18de91a1d0df2826d
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2023-07-16 20:28:52 -0300

    adicionando pagina do FAQ

--- 01-nextjs-course-aula2.5/pages/faq.js
+++ 01-nextjs-course-aula2.5/pages/faq.js
@@ -1,5 +1,9 @@
-import Head from 'next/head'
-import Link from '../src/components/Link';
+// import Head from 'next/head'
+// import Link from '../src/components/Link';
+
+import FAQScreen from '../src/screens/FaqScreen/index.js';
+
+export default FAQScreen; 
 
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json '
@@ -19,42 +23,42 @@ export async function getStaticProps() {
 }
 
 
-export default function FAQPage({faq}) {
-console.log(faq);
-    // const [faq, setFaq] = useState([])
+// export default function FAQPage({faq}) {
+// console.log(faq);
+//     // const [faq, setFaq] = useState([])
 
-    // useEffect(() => {
-        // const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json '
-        // fetch(FAQ_API_URL)
-        //     .then((respostaDoServidor) => {
-        //         return respostaDoServidor.json();
+//     // useEffect(() => {
+//         // const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json '
+//         // fetch(FAQ_API_URL)
+//         //     .then((respostaDoServidor) => {
+//         //         return respostaDoServidor.json();
 
-        //     })
-        //     .then((resposta) => {
-        //         setFaq(resposta);
-        //     })
-    // }, [])
-    return (
-        <div>
-            <Head>
-                <title>Faq - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
+//         //     })
+//         //     .then((resposta) => {
+//         //         setFaq(resposta);
+//         //     })
+//     // }, [])
+//     return (
+//         <div>
+//             <Head>
+//                 <title>Faq - Alura Cases Campanha</title>
+//             </Head>
+//             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+//             <Link href="/">
+//                 Ir para a home
+//             </Link>
+//             <ul>
+//                 {faq.map(({ answer, question }) => (
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
 
 
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
